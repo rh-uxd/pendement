7fbc626c0b71e169b69eb46ec74745c8f03f3f39
Joachim Schuler <jschuler@redhat.com>
2020-11-03 15:14:50 -0500

    login identify

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const Analytics = require("analytics-node");
 // initialize with our Segment write key
 const analytics = new Analytics("37ZPGCCCslQLfvepLtkAqj2yFtvT9fdD");
 
+// track users in memory to know whether they are new or pre-existing
+const allUsers = {};
+
 // Configure express to use body-parser as middle-ware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -15,33 +18,59 @@ app.use(bodyParser.json());
 // use the express-static middleware
 app.use(express.static("public"));
 
+function createUser(id) {
+  if (allUsers[id]) {
+    console.log(
+      `existing id: ${id}, name: ${allUsers[id].name}, email: ${allUsers[id].email}`
+    );
+    return "existing";
+  } else {
+    const idCapitalized = id.charAt(0).toUpperCase() + id.slice(1);
+    const name = `${idCapitalized} Smith`;
+    const email = `${id}@example.com`;
+    /*
+      identify lets you tie a user to their actions and record traits about them. 
+      It includes a unique User ID and/or anonymous ID, and any optional traits you know about them.
+      You should call identify once when the user’s account is first created, and then again any time their traits change.
+      */
+    analytics.identify({
+      userId: id,
+      traits: {
+        name,
+        email,
+      },
+    });
+    allUsers[id] = {
+      name,
+      email,
+    };
+    console.log(`new id: ${id}, name: ${name}, email: ${email}`);
+    return "new";
+  }
+}
+
 // POST method route
 app.post("/api/login", function (req, res) {
   const { username } = req.body;
+  const type = createUser(username);
   res.json({
-    id: `${username}-id`,
+    id: username,
+    name: allUsers[id].name,
+    email: allUsers[id].email,
+    type,
   });
 });
 
 // http://localhost:3000/api/create/?id=user1
 app.get("/api/create", function (req, res) {
   const id = req.query.id;
-  const idCapitalized = id.charAt(0).toUpperCase() + id.slice(1);
-  const name = `${idCapitalized} Smith`;
-  const email = `${id}@example.com`;
-  /*
-  identify lets you tie a user to their actions and record traits about them. 
-  It includes a unique User ID and/or anonymous ID, and any optional traits you know about them.
-  You should call identify once when the user’s account is first created, and then again any time their traits change.
-  */
-  analytics.identify({
-    userId: id,
-    traits: {
-      name,
-      email,
-    },
+  const type = createUser(id);
+  res.json({
+    id,
+    name: allUsers[id].name,
+    email: allUsers[id].email,
+    type,
   });
-  res.send(`identify id: ${id}, name: ${name}, email: ${email}`);
 });
 
 // http://localhost:3000/api/track/?id=user1&event=login
@@ -55,7 +84,7 @@ app.get("/api/track", function (req, res) {
     */
   analytics.track({
     userId: id,
-    event
+    event,
   });
   res.send(`track id: ${id}, event: ${event}`);
 });

637e49882f9af88b73a7f6cf0d78c8d7ab01a325
redallen <zallen@redhat.com>
2020-11-03 12:32:28 -0500

    add route html

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // create an express app
 const express = require("express")
-const app = express()
+const app = express();
 const Analytics = require('analytics-node');
 
 // https://segment.com/docs/connections/sources/catalog/libraries/server/node/quickstart/
@@ -8,12 +8,12 @@ const Analytics = require('analytics-node');
 const analytics = new Analytics('37ZPGCCCslQLfvepLtkAqj2yFtvT9fdD');
 
 // use the express-static middleware
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 // define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-})
+app.get("/api-request", function (req, res) {
+  res.send(JSON.stringify({ status: 'success' }));
+});
 
 // start the server listening for requests
 const port = process.env.PORT || 3000;

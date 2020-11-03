0be769e26c6c5cd78b28c3f20782bd9dce8ee1cf
Joachim Schuler <jschuler@redhat.com>
2020-11-03 11:01:46 -0500

    segment analytics

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 // create an express app
 const express = require("express")
 const app = express()
+const Analytics = require('analytics-node');
+
+// https://segment.com/docs/connections/sources/catalog/libraries/server/node/quickstart/
+// initialize with our Segment write key
+const analytics = new Analytics('37ZPGCCCslQLfvepLtkAqj2yFtvT9fdD');
 
 // use the express-static middleware
 app.use(express.static("public"))
